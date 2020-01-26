34e74ce39b6bd00f9356a139c2bd54d48569d436
viman39 <vimanmanuel@gmail.com>
2020-01-26 18:39:59 +0200

    login + register

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
     res.render('index.ejs')
 });
 
+router.get('/login', (req, res) => {
+    res.render("login.ejs")
+})
+
+router.get('/register', (req, res) => {
+    res.render("register.ejs")
+})
+
 module.exports = router
